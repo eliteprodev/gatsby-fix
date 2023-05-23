0cbbde666e0c0c763918e5407a55628d58f9a20b
spectrepro <warnold233@gmail.com>
2023-05-22 21:05:16 -0400

    fix bugs for build

--- gatsby-node.js
+++ gatsby-node.js
@@ -62,6 +62,20 @@ module.exports.onCreateWebpackConfig = ({
         node: 'node'
       })
     ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
   })
 
   // Silence 'conflicting order' warning for CSS modules.
